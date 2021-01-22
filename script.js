e93cb4a9da6b7f94f47f929de9c28d75a476bb8c
sulaksulakteja <75995019+sulaksulakteja@users.noreply.github.com>
2021-01-22 10:44:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if (url.searchParams.get('by') != null) {
 
 
 document.querySelector(".tombol").addEventListener('click', function () {
-  Swal.fire("Hallo calon pacar", "aku ada pertanyaan nih buat kamu", "question").then(function () {
+  Swal.fire("Hallo calon pacar aku", "aku ada pertanyaan nih buat kamu", "question").then(function () {
     Swal.fire("Jawab yang jujur ya!").then(function () {
       Swal.fire("Awas aja kalau kamu bohong", "", "error").then(function () {
 
